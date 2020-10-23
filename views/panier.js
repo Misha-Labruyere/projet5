8f122d88e426fc15f176038faa8a96c9eaf73896
Misha-Labruyere <mishalabruyere@gmail.com>
2020-10-23 13:37:17 +0200

    check up

--- views/panier.js
+++ views/panier.js
@@ -2,8 +2,13 @@ const infoPanier = document.getElementById("info_panier");
 const infoCamera = localStorage.getItem("selectedCameras");
 const infoCameras = JSON.parse(infoCamera);
 
-for (const camera of infoCameras) {
+for (const [index, camera] of infoCameras.entries()) {
   const recupId = camera.idProduct;
+
+  if (recupId !== null)  {
+  
+  }
+
 
   fetch("http://localhost:3000/api/cameras/" + recupId)
     .then((product) => product.json())
@@ -12,11 +17,11 @@ for (const camera of infoCameras) {
       console.log(product);
       let detailProduct = `<div class="info_panier">
   <img class="photo" src="${product.imageUrl}">
-  <div class="id">${product.id}</div>
+  <div class="lens">${product.choiceProduct}</div>
   <div class="name">${product.name}</div>
   <div class="price">${product.price}</div>
   <div class="description">${product.description}</div>
-  <button onclick="delete_product('${product._id}')" class="suppression_article">Supprimer du panier</button>
+  <button onclick="delete_product(${index})" class="suppression_article">Supprimer du panier</button>
   </div>`;
       const productPanier = document.getElementById("selection_panier");
       productPanier.innerHTML += detailProduct;
@@ -28,19 +33,18 @@ for (const camera of infoCameras) {
 
 function delete_product(id) {
   let panier = JSON.parse(localStorage.getItem("selectedCameras"));
+  window.location.reload();
   //supprimer le produit dans le tableau
 
   let newPanier = [];
 
-  for (const produit of panier) {
-    console.log(produit);
-console.log(id, produit.idProduct);
-    if (id !== produit.idProduct) {
+  for (const [index, produit] of panier.entries()) {
+    console.log(index, produit);
+    console.log(id, produit.idProduct);
+    if (id !== index) {
       newPanier.push(produit);
     }
   }
 
   localStorage.setItem("selectedCameras", JSON.stringify(newPanier));
 }
-
-
